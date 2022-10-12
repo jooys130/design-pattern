fa7e6b9fae31e3a342e7374e5a64cfa61233dd12
jooys130 <wndustn130@gmail.com>
2022-10-12 17:43:32 +0900

    chap2: Practice 1
    reason using Instance type

--- src/Adaptor/Ex1Class/Main.java
+++ src/Adaptor/Ex1Class/Main.java
@@ -2,6 +2,10 @@ package Adaptor.Ex1Class;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        PrintBanner 클래스의 인스턴스는 Print 형의 변수에 대입
+        ㄴ why? PrintBanner 클래스의 메소드가 아닌 Print 인터페이스의 메소드를 이용하고 있다 (*프로그램의 의도*)
+         */
         Print p = new PrintBanner("Hello");
         p.printWeak();
         p.printStrong();

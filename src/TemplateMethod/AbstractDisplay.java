e30508b2bc151f492d096d6e2bacaa89a3caf60a
jooys130 <wndustn130@gmail.com>
2022-10-13 14:23:25 +0900

    chap3: Template Method Practice 2
    use of 'final'

--- src/TemplateMethod/AbstractDisplay.java
+++ src/TemplateMethod/AbstractDisplay.java
@@ -12,6 +12,10 @@ public abstract class AbstractDisplay {
     public abstract void close();
 
     public final void display(){ // 추상 클래스에서 구현되고 있는 메소드 display
+        /*
+            final 선언을 통해 하위 클래스에서 오버라이드를 금지한다
+            : 하위 클래스를 만드려면 display 메서드를 오버라이드하지 말고 기능을 확장해라
+         */
         open(); // this.open()
 
         for(int i = 0; i < 5; ++i){

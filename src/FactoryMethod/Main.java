79b42e24b113eff3589af712b92531b3425bb2ba
jooys130 <wndustn130@gmail.com>
2022-10-13 17:00:07 +0900

    chap4: Factory Method
    variable name changes

--- src/FactoryMethod/Main.java
+++ src/FactoryMethod/Main.java
@@ -8,12 +8,12 @@ public class Main {
     }
 
     public static void main(String[] var0) {
-        IDCardFactory var1 = new IDCardFactory();
-        Product var2 = var1.create("홍길동");
-        Product var3 = var1.create("이순신");
-        Product var4 = var1.create("강감찬");
-        var2.use();
-        var3.use();
-        var4.use();
+        IDCardFactory factory = new IDCardFactory();
+        Product card1 = factory.create("홍길동");
+        Product card2 = factory.create("이순신");
+        Product card3 = factory.create("강감찬");
+        card1.use();
+        card2.use();
+        card3.use();
     }
 }

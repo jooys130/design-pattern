af47484aa974dc0e766bda34a6781ec236e9ec13
jooys130 <wndustn130@gmail.com>
2022-10-13 10:55:45 +0900

    chap2: Practice 2 Answer

--- src/Adaptor/Practice2/Main.java
+++ src/Adaptor/Practice2/Main.java
@@ -6,11 +6,13 @@ public class Main {
     public static void main(String[] args) {
         FileIO f = new FileProperties();
         try {
-            f.readFromFile("file.txt");
-            f.setValue("year", "2004");
-            f.setValue("month", "4");
-            f.setValue("day", "21");
-            f.writeToFile("newfile.txt");
+            f.readFromFile("D:\\DesignPattern\\src\\Adaptor\\Practice2\\file.txt");
+            f.setValue("year", "2001");
+            f.setValue("month", "1");
+            f.setValue("day", "30");
+            // getValue Test
+            System.out.println(f.getValue("year"));
+            f.writeToFile("D:\\DesignPattern\\src\\Adaptor\\Practice2\\test.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }

530b2bd9eae2f832b4814acb650cd1198e54c925
jooys130 <wndustn130@gmail.com>
2022-10-14 14:51:11 +0900

    chap5: Factory Method Practice 1

--- src/FactoryMethod/idcard/IDCard.java
+++ src/FactoryMethod/idcard/IDCard.java
@@ -4,6 +4,11 @@ import FactoryMethod.framework.*;
 public class IDCard extends Product{
     private String owner;
 
+    /*
+        아무런 접근 제어자(ex. public, protected, private)가 붙어있지 않다
+        -> 동일한 패키지 내의 클래스 내에서만 사용 가능하므로 idcard 패키지 외부에서 인스턴스를 생성할 수 없다
+           즉, IDCardFactory를 경유해야한다
+     */
     IDCard(String owner) {
         System.out.println(owner + "의 카드를 만듭니다.");
         this.owner = owner;

6f4d5e3150be8fa6e32cfdea2c61985bd998c6f8
jooys130 <wndustn130@gmail.com>
2022-10-19 10:10:55 +0900

    chap7: Builder Practie 1

--- src/Builder/Builder.java
+++ src/Builder/Builder.java
@@ -1,6 +1,13 @@
 package Builder;
 
+/*
+    (7-1) Builder 클래스를 interface로 변경한다면?
+        Builder 클래스: abstract class -> interface
+        TexBuilder, HTMLBulder 클래스: extends -> implements
+ */
+
 public abstract class Builder {
+    // abstract class -> interface
     public abstract void makeTitle(String title);
     public abstract void makeString(String str);
     public abstract void makeItems(String[] items);
